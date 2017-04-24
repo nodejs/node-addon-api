8dfc97a67268c253387f465c31e84a6b0f01cae8
Jason Ginchereau <jasongin@microsoft.com>
2017-04-24 14:21:17 -0700

    Add PropertyDescriptor creation methods
    Add static methods on the PropertyDescriptor class that provide
    convenient ways to construct PropertyDescriptor instances for different
    kinds of property descriptors. These are equivalent to the similar
    static methods on the ObjectWrap class, except they are for use when
    defining properties outside of an object-wrapping scenario. Callbacks
    are templatized (like with Function::New()) so that they can work
    with lambdas or regular function pointers.
    
    New test cases cover the various overloads for constructing property
    descriptors and defining properties.
    
    And the Name class constructors needed to be made public, to allow
    code like value.As<Name>(), that is used by the test.

---
+++ test/object.cc
@@ -0,0 +1,80 @@
+#include "napi.h"
+
+using namespace Napi;
+
+static bool testValue = true;
+
+Value TestGetter(const CallbackInfo& info) {
+   return Boolean::New(info.Env(), testValue);
+}
+
+void TestSetter(const CallbackInfo& info) {
+   testValue = info[0].As<Boolean>();
+}
+
+Value TestFunction(const CallbackInfo& info) {
+   return Boolean::New(info.Env(), true);
+}
+
+void DefineProperties(const CallbackInfo& info) {
+  Object obj = info[0].As<Object>();
+  String nameType = info[1].As<String>();
+
+  Boolean trueValue = Boolean::New(info.Env(), true);
+
+  if (nameType.Utf8Value() == "literal") {
+    obj.DefineProperties({
+      PropertyDescriptor::Accessor("readonlyAccessor", TestGetter),
+      PropertyDescriptor::Accessor("readwriteAccessor", TestGetter, TestSetter),
+      PropertyDescriptor::Value("readonlyValue", trueValue),
+      PropertyDescriptor::Value("readwriteValue", trueValue, napi_writable),
+      PropertyDescriptor::Value("enumerableValue", trueValue, napi_enumerable),
+      PropertyDescriptor::Value("configurableValue", trueValue, napi_configurable),
+      PropertyDescriptor::Function("function", TestFunction),
+    });
+  } else if (nameType.Utf8Value() == "string") {
+    obj.DefineProperties({
+      PropertyDescriptor::Accessor(std::string("readonlyAccessor"), TestGetter),
+      PropertyDescriptor::Accessor(std::string("readwriteAccessor"), TestGetter, TestSetter),
+      PropertyDescriptor::Value(std::string("readonlyValue"), trueValue),
+      PropertyDescriptor::Value(std::string("readwriteValue"), trueValue, napi_writable),
+      PropertyDescriptor::Value(std::string("enumerableValue"), trueValue, napi_enumerable),
+      PropertyDescriptor::Value(std::string("configurableValue"), trueValue, napi_configurable),
+      PropertyDescriptor::Function(std::string("function"), TestFunction),
+    });
+  } else if (nameType.Utf8Value() == "value") {
+    obj.DefineProperties({
+      PropertyDescriptor::Accessor(
+        Napi::String::New(info.Env(), "readonlyAccessor"), TestGetter),
+      PropertyDescriptor::Accessor(
+        Napi::String::New(info.Env(), "readwriteAccessor"), TestGetter, TestSetter),
+      PropertyDescriptor::Value(
+        Napi::String::New(info.Env(), "readonlyValue"), trueValue),
+      PropertyDescriptor::Value(
+        Napi::String::New(info.Env(), "readwriteValue"), trueValue, napi_writable),
+      PropertyDescriptor::Value(
+        Napi::String::New(info.Env(), "enumerableValue"), trueValue, napi_enumerable),
+      PropertyDescriptor::Value(
+        Napi::String::New(info.Env(), "configurableValue"), trueValue, napi_configurable),
+      PropertyDescriptor::Function(
+        Napi::String::New(info.Env(), "function"), TestFunction),
+    });
+  }
+}
+
+void DefineValueProperty(const CallbackInfo& info) {
+  Object obj = info[0].As<Object>();
+  Name name = info[1].As<Name>();
+  Value value = info[2];
+
+  obj.DefineProperty(PropertyDescriptor::Value(name, value));
+}
+
+Object InitObject(Env env) {
+  Object exports = Object::New(env);
+
+  exports["defineProperties"] = Function::New(env, DefineProperties);
+  exports["defineValueProperty"] = Function::New(env, DefineValueProperty);
+
+  return exports;
+}
---
+++ test/object.js
@@ -0,0 +1,71 @@
+'use strict';
+const buildType = process.config.target_defaults.default_configuration;
+const binding = require(`./build/${buildType}/binding.node`);
+const assert = require('assert');
+
+function assertPropertyIs(obj, key, attribute) {
+   const propDesc = Object.getOwnPropertyDescriptor(obj, key);
+   assert.ok(propDesc);
+   assert.ok(propDesc[attribute]);
+}
+
+function assertPropertyIsNot(obj, key, attribute) {
+   const propDesc = Object.getOwnPropertyDescriptor(obj, key);
+   assert.ok(propDesc);
+   assert.ok(!propDesc[attribute]);
+}
+
+function testDefineProperties(nameType) {
+  const obj = {};
+  binding.object.defineProperties(obj, nameType);
+
+  assertPropertyIsNot(obj, 'readonlyAccessor', 'writable');
+  assertPropertyIsNot(obj, 'readonlyAccessor', 'enumerable');
+  assertPropertyIsNot(obj, 'readonlyAccessor', 'configurable');
+  assert.strictEqual(obj.readonlyAccessor, true);
+
+  assertPropertyIs(obj, 'readwriteAccessor', 'writable');
+  assertPropertyIsNot(obj, 'readwriteAccessor', 'enumerable');
+  assertPropertyIsNot(obj, 'readwriteAccessor', 'configurable');
+  obj.readwriteAccessor = false;
+  assert.strictEqual(obj.readwriteAccessor, false);
+  obj.readwriteAccessor = true;
+  assert.strictEqual(obj.readwriteAccessor, true);
+
+  assertPropertyIsNot(obj, 'readonlyValue', 'writable');
+  assertPropertyIsNot(obj, 'readonlyValue', 'enumerable');
+  assertPropertyIsNot(obj, 'readonlyValue', 'configurable');
+  assert.strictEqual(obj.readonlyValue, true);
+
+  assertPropertyIs(obj, 'readwriteValue', 'writable');
+  assertPropertyIsNot(obj, 'readwriteValue', 'enumerable');
+  assertPropertyIsNot(obj, 'readwriteValue', 'configurable');
+  obj.readwriteValue = false;
+  assert.strictEqual(obj.readwriteValue, false);
+  obj.readwriteValue = true;
+  assert.strictEqual(obj.readwriteValue, true);
+
+  assertPropertyIsNot(obj, 'enumerableValue', 'writable');
+  assertPropertyIs(obj, 'enumerableValue', 'enumerable');
+  assertPropertyIsNot(obj, 'enumerableValue', 'configurable');
+
+  assertPropertyIsNot(obj, 'configurableValue', 'writable');
+  assertPropertyIsNot(obj, 'configurableValue', 'enumerable');
+  assertPropertyIs(obj, 'configurableValue', 'configurable');
+
+  assertPropertyIsNot(obj, 'function', 'writable');
+  assertPropertyIsNot(obj, 'function', 'enumerable');
+  assertPropertyIsNot(obj, 'function', 'configurable');
+  assert.strictEqual(obj.function(), true);
+}
+
+testDefineProperties('literal');
+testDefineProperties('string');
+testDefineProperties('value');
+
+{
+  const obj = {};
+  const testSym = Symbol();
+  binding.object.defineValueProperty(obj, testSym, 1);
+  assert.strictEqual(obj[testSym], 1);
+}
