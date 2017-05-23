f797d1495e0dedb719c5ce5dfe522582b7416c49
Gabriel "_|Nix|_" Schulhof <gabriel.schulhof@intel.com>
2017-05-23 19:26:54 +0300

    Include-path-juggle node_api.h instead of napi.h (#41) (#42)
    Instead of making the path from which napi.h is included conditional
    upon the Node.js version, make the path of node_api.h thusly
    conditional. This way, no additional modification needs to be made to a
    dependent's binding.gyp in order to add the EXTERNAL_NAPI preprocessor
    directive when the C++ wrappers are not used.
    
    By this mechanism the C++ wrapper napi.h will also refer to the correct
    version of node_api.h.
    
    Fixes https://github.com/nodejs/node-api/issues/41

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var path = require('path');
 // or detect presence of built-in N-API by some other mechanism TBD.
 var isNodeApiBuiltin = process.versions.modules >= 52;  // Node 8.x
 
-var include = __dirname;
+var include = path.join(__dirname, 'src');
 var gyp = path.join(__dirname, 'src', 'node_api.gyp');
 
 if (isNodeApiBuiltin) {
    gyp += ':nothing';
 } else {
    gyp += ':node-api';
-   include = path.join(__dirname, 'src');
+   include = path.join(__dirname, 'external-napi');
 }
 
 module.exports = {
-   include: include,
+   include: [ '"' + include + '"', '"' + __dirname + '"' ].join(' '),
    gyp: gyp,
    isNodeApiBuiltin: isNodeApiBuiltin,
 };
--- src/napi.h
+++
@@ -1,2 +0,0 @@
-#define EXTERNAL_NAPI
-#include "../napi.h"
