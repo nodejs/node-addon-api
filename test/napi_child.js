6887110287ce01a774ca4f9e537ce87dbd42ec35
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-08-25 13:08:48 +0300

    Introduce flag check
    The version starting with which N-API is considered built-in is not the
    same as the version starting with which the --napi-modules flag is
    consisdered dropped. The latter needs a separate check.
    
    Fixes: https://github.com/nodejs/node-addon-api/issues/119
    PR-URL: https://github.com/nodejs/node-addon-api/pull/121

--- test/napi_child.js
+++ test/napi_child.js
@@ -1,6 +1,6 @@
 // Makes sure that child processes are spawned appropriately.
 exports.spawnSync = function(command, args, options) {
-  if (require('../index').isNodeApiBuiltin) {
+  if (require('../index').needsFlag) {
     args.splice(0, 0, '--napi-modules');
   }
   return require('child_process').spawnSync(command, args, options);
