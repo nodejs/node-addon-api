a870338b4d77e23fed6040a63dc00992afca5d1c
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-08-25 13:08:48 +0300

    Introduce flag check
    The version starting with which N-API is considered built-in is not the
    same as the version starting with which the --napi-modules flag is
    consisdered dropped. The latter needs a separate check.
    
    PR-URL: https://github.com/nodejs/node-addon-api/pull/121
    Fixes: https://github.com/nodejs/node-addon-api/issues/119
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var path = require('path');
 
-// We know which version of Node.js first shipped the incarnation of the API
-// available in *this* package. So, if we find that the Node.js version is below
-// that, we indicate that the API is missing from Node.js.
-function getNodeApiBuiltin() {
-  var versionArray = process.version
-    .substr(1)
-    .split('.')
-    .map(function(item) {
-      return +item;
-    });
-  return versionArray[0] >= 8 && versionArray[1] >= 4 && versionArray[2] >= 0;
-}
+var versionArray = process.version
+  .substr(1)
+  .replace(/-.*$/, '')
+  .split('.')
+  .map(function(item) {
+    return +item;
+  });
 
 // TODO: Check if the main node semantic version is within multiple ranges,
 // or detect presence of built-in N-API by some other mechanism TBD.
-var isNodeApiBuiltin = getNodeApiBuiltin();
+
+// We know which version of Node.js first shipped the incarnation of the API
+// available in *this* package. So, if we find that the Node.js version is below
+// that, we indicate that the API is missing from Node.js.
+var isNodeApiBuiltin =
+  (versionArray[0] >= 8 && versionArray[1] >= 4 && versionArray[2] >= 0);
+
+// So far it looks like even version 9 will need the flag. We need to adjust
+// this for the version where the flag is dropped whenever that version lands.
+var needsFlag = (versionArray[0] >= 8);
 
 var include = path.join(__dirname, 'src');
 var gyp = path.join(__dirname, 'src', 'node_api.gyp');
@@ -31,4 +35,5 @@ module.exports = {
    include: [ '"' + include + '"', '"' + __dirname + '"' ].join(' '),
    gyp: gyp,
    isNodeApiBuiltin: isNodeApiBuiltin,
+   needsFlag: needsFlag
 };
