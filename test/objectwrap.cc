b96e7d142cbaacd23e87723e1ac1f1bf8cf4184d
David Halls <dahalls@gmail.com>
2017-09-03 10:38:58 +0100

    Reformat objectwrap fix and test

--- test/objectwrap.cc
+++ test/objectwrap.cc
@@ -1,40 +1,32 @@
 #include <napi.h>
 
-class Test : public Napi::ObjectWrap<Test>
-{
+class Test : public Napi::ObjectWrap<Test> {
 public:
-    Test(const Napi::CallbackInfo& info) :
-        Napi::ObjectWrap<Test>(info)
-    {
-    }
+  Test(const Napi::CallbackInfo& info) :
+    Napi::ObjectWrap<Test>(info) {
+  }
 
-    void Set(const Napi::CallbackInfo& info)
-    {
-        value = info[0].As<Napi::Number>();
-    }
+  void Set(const Napi::CallbackInfo& info) {
+    value = info[0].As<Napi::Number>();
+  }
 
-    Napi::Value Get(const Napi::CallbackInfo& info)
-    {
-        return Napi::Number::New(info.Env(), value);
-    }
+  Napi::Value Get(const Napi::CallbackInfo& info) {
+    return Napi::Number::New(info.Env(), value);
+  }
 
-    static void Initialize(Napi::Env env, Napi::Object exports)
-    {
-        exports.Set("Test", DefineClass(env, "Test",
-        {
-            InstanceMethod("test_set", &Test::Set),
-            InstanceMethod("test_get", &Test::Get)
-        }));
-    }
+  static void Initialize(Napi::Env env, Napi::Object exports) {
+    exports.Set("Test", DefineClass(env, "Test", {
+      InstanceMethod("test_set", &Test::Set),
+      InstanceMethod("test_get", &Test::Get)
+    }));
+  }
 
 private:
-    uint32_t value;
+  uint32_t value;
 };
 
-Napi::Object InitObjectWrap(Napi::Env env)
-{
-    Napi::Object exports = Napi::Object::New(env);
-    Test::Initialize(env, exports);
-    return exports;
+Napi::Object InitObjectWrap(Napi::Env env) {
+  Napi::Object exports = Napi::Object::New(env);
+  Test::Initialize(env, exports);
+  return exports;
 }
-
--- test/objectwrap.js
+++ test/objectwrap.js
@@ -5,23 +5,20 @@ const assert = require('assert');
 test(require(`./build/${buildType}/binding.node`));
 test(require(`./build/${buildType}/binding_noexcept.node`));
 
-function test(binding)
-{
-    var Test = binding.objectwrap.Test;
+function test(binding) {
+  var Test = binding.objectwrap.Test;
 
-    function testSetGet(obj)
-    {
-        obj.test_set(90);
-        assert.strictEqual(obj.test_get(), 90);
-    }
+  function testSetGet(obj) {
+    obj.test_set(90);
+    assert.strictEqual(obj.test_get(), 90);
+  }
 
-    function testObj(obj)
-    {
-        testSetGet(obj);
-    }
+  function testObj(obj) {
+    testSetGet(obj);
+  }
 
-    testObj(new Test());
-    testObj(new Test(1));
-    testObj(new Test(1, 2, 3, 4, 5, 6));
-    testObj(new Test(1, 2, 3, 4, 5, 6, 7));
+  testObj(new Test());
+  testObj(new Test(1));
+  testObj(new Test(1, 2, 3, 4, 5, 6));
+  testObj(new Test(1, 2, 3, 4, 5, 6, 7));
 }
