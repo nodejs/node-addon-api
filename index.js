646e1b68039d3271f58fa713f36ee88fbbcabb37
Hitesh Kanwathirtha <hiteshk@microsoft.com>
2017-09-18 18:08:35 -0700

    Fix Node n-api support check
    PR-URL: https://github.com/nodejs/node-addon-api/pull/141
    Reviewed-By: Gabriel Schulhof <gabriel.schulhof@intel.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var versionArray = process.version
 // available in *this* package. So, if we find that the Node.js version is below
 // that, we indicate that the API is missing from Node.js.
 var isNodeApiBuiltin =
-  (versionArray[0] >= 8 && versionArray[1] >= 4 && versionArray[2] >= 0);
+  (versionArray[0] > 8 || (versionArray[0] == 8 && versionArray[1] > 5));
 
 // So far it looks like even version 9 will need the flag. We need to adjust
 // this for the version where the flag is dropped whenever that version lands.
