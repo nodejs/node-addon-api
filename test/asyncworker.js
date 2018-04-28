d9dbe05d4217c0a48dd0a3b94e623f5cee3d91c2
Jinho Bang <zino@chromium.org>
2018-04-28 18:05:34 +0900

    Add resource_name and resource parameters to AsyncWorker constructor
    This change is initiated from https://github.com/nodejs/node-addon-api/pull/140#discussion_r140302349.

--- test/asyncworker.js
+++ test/asyncworker.js
@@ -1,21 +1,88 @@
 'use strict';
 const buildType = process.config.target_defaults.default_configuration;
 const assert = require('assert');
+const async_hooks = require('async_hooks');
+const common = require('./common');
 
 test(require(`./build/${buildType}/binding.node`));
 test(require(`./build/${buildType}/binding_noexcept.node`));
 
-function test(binding) {
-  binding.asyncworker.doWork(true, function (e) {
-    assert.strictEqual(typeof e, 'undefined');
-    assert.strictEqual(typeof this, 'object');
-    assert.strictEqual(this.data, 'test data');
-  }, 'test data');
+function installAsyncHooksForTest() {
+  return new Promise((resolve, reject) => {
+    let id;
+    const events = [];
+    const hook = async_hooks.createHook({
+      init(asyncId, type, triggerAsyncId, resource) {
+        if (id === undefined && type === 'TestResource') {
+          id = asyncId;
+          events.push({ eventName: 'init', type, triggerAsyncId, resource });
+        }
+      },
+      before(asyncId) {
+        if (asyncId === id) {
+          events.push({ eventName: 'before' });
+        }
+      },
+      after(asyncId) {
+        if (asyncId === id) {
+          events.push({ eventName: 'after' });
+        }
+      },
+      destroy(asyncId) {
+        if (asyncId === id) {
+          events.push({ eventName: 'destroy' });
+          hook.disable();
+          resolve(events);
+        }
+      }
+    }).enable();
+  });
+}
 
-  binding.asyncworker.doWork(false, function (e) {
-    assert.ok(e instanceof Error);
-    assert.strictEqual(e.message, 'test error');
-    assert.strictEqual(typeof this, 'object');
-    assert.strictEqual(this.data, 'test data');
-  }, 'test data');
+function test(binding) {
+  {
+    const hooks = installAsyncHooksForTest();
+    const triggerAsyncId = async_hooks.executionAsyncId();
+    binding.asyncworker.doWork(true, { foo: 'foo' }, function (e) {
+      assert.strictEqual(typeof e, 'undefined');
+      assert.strictEqual(typeof this, 'object');
+      assert.strictEqual(this.data, 'test data');
+    }, 'test data');
+
+    hooks.then(actual => {
+      assert.deepStrictEqual(actual, [
+        { eventName: 'init',
+          type: 'TestResource',
+          triggerAsyncId: triggerAsyncId,
+          resource: { foo: 'foo' } },
+        { eventName: 'before' },
+        { eventName: 'after' },
+        { eventName: 'destroy' }
+      ]);
+    }).catch(common.mustNotCall());
+  }
+
+  {
+    const hooks = installAsyncHooksForTest();
+    const triggerAsyncId = async_hooks.executionAsyncId();
+
+    binding.asyncworker.doWork(false, { foo: 'foo' }, function (e) {
+      assert.ok(e instanceof Error);
+      assert.strictEqual(e.message, 'test error');
+      assert.strictEqual(typeof this, 'object');
+      assert.strictEqual(this.data, 'test data');
+    }, 'test data');
+
+    hooks.then(actual => {
+      assert.deepStrictEqual(actual, [
+        { eventName: 'init',
+          type: 'TestResource',
+          triggerAsyncId: triggerAsyncId,
+          resource: { foo: 'foo' } },
+        { eventName: 'before' },
+        { eventName: 'after' },
+        { eventName: 'destroy' }
+      ]);
+    }).catch(common.mustNotCall());
+  }
 }
