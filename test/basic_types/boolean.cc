14c69abd46787b89b125e9c5bde6616385f7750b
Jaeseok Yoon <yjaeseok@gmail.com>
2018-09-06 09:42:27 +0900

    test: write tests for Boolean class
    The new tests cover a part of the basic type conversion from JavaScript
    type to native type.
    
    PR-URL: https://github.com/nodejs/node-addon-api/pull/328
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Nicola Del Gobbo <nicoladelgobbo@NickNaso.local>

---
+++ test/basic_types/boolean.cc
@@ -0,0 +1,15 @@
+#include "napi.h"
+
+using namespace Napi;
+
+Value CreateBoolean(const CallbackInfo& info) {
+  return Boolean::New(info.Env(), info[0].As<Boolean>().Value());
+}
+
+Object InitBasicTypesBoolean(Env env) {
+  Object exports = Object::New(env);
+
+  exports["createBoolean"] = Function::New(env, CreateBoolean);
+
+  return exports;
+}
