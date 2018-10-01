c814903244eef135b648adbffd479a5894d47fd3
Jaeseok Yoon <yjaeseok@gmail.com>
2018-10-02 08:42:54 +0900

    Added TestCase for Utf16 string

--- test/basic_types/string.js
+++ test/basic_types/string.js
@@ -6,6 +6,9 @@ test(require(`../build/${buildType}/binding.node`));
 test(require(`../build/${buildType}/binding_noexcept.node`));
 
 function test(binding) {
-	const testString = binding.basic_types_string.createUtf8String("κόσμε");
-	assert.strictEqual(testString, "κόσμε");
+	const testUtf8String = binding.basic_types_string.createUtf8String("κόσμε");
+	assert.strictEqual(testUtf8String, "κόσμε");
+
+	const testUtf16String = binding.basic_types_string.createUtf16String("κόσμε");
+	assert.strictEqual(testUtf16String, "κόσμε");
 }
