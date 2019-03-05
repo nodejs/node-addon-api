96eece45b54dc7e2d281ea3258cfcb47c7a8969c
Dongjin Na <dongjinna@gmail.com>
2019-03-05 22:04:45 +0900

    Update cleanuphook.cc
    Fix the i

--- test/cleanuphook.cc
+++ test/cleanuphook.cc
@@ -7,16 +7,16 @@ using namespace Napi;
 class TestCleanupHook : public CleanupHook {
   public:
     static void DoCleanup(const CallbackInfo& info) {
-	  static int secret = 42;
-	  static int wrongSecret = 17;
-	  TestCleanupHook* testCleanupHook = new TestCleanupHook(info, wrongSecret);
-	  delete testCleanupHook;
+      static int secret = 42;
+      static int wrongSecret = 17;
+      TestCleanupHook* testCleanupHook = new TestCleanupHook(info, wrongSecret);
+      delete testCleanupHook;
 
       testCleanupHook = new TestCleanupHook(info, secret);
-	}
-	void Cleanup() {
-	  printf("cleanup(%d)\n", _secret);
-	};
+    }
+    void Cleanup() {
+      printf("cleanup(%d)\n", _secret);
+    };
   private:
     TestCleanupHook(const CallbackInfo& info, int secret) : CleanupHook(info.Env()), _secret(secret) {}
     int _secret;
