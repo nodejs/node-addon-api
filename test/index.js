0e9d4aeb6e35835466b00de5a89008e5f2df2101
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2019-10-24 21:04:18 +0200

    test: fix tsfn tests by napi version

--- test/index.js
+++ test/index.js
@@ -70,6 +70,8 @@ if ((process.env.npm_config_NAPI_VERSION !== undefined) &&
 if ((process.env.npm_config_NAPI_VERSION !== undefined) &&
     (process.env.npm_config_NAPI_VERSION < 4)) {
   testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_ptr'), 1);
+  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_sum'), 1);
+  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_unref'), 1);
   testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function'), 1);
 }
 
