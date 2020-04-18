89620dd6f3875434fe59ae879d298201e481718b
Yulong Wang <yulongw@microsoft.com>
2020-04-18 12:00:47 -0700

    support bigint64/biguint64 in TypedArray::ElementSize()

--- napi-inl.h
+++ napi-inl.h
@@ -1628,6 +1628,10 @@ inline uint8_t TypedArray::ElementSize() const {
     case napi_float32_array:
       return 4;
     case napi_float64_array:
+#ifdef NAPI_EXPERIMENTAL
+    case napi_bigint64_array:
+    case napi_biguint64_array:
+#endif  // NAPI_EXPERIMENTAL
       return 8;
     default:
       return 0;
