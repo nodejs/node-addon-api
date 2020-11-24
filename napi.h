836a9bb6923773ca7736f0b50fd86d04a19fdcea
NickNaso <nicoladelgobbo@gmail.com>
2020-11-24 13:08:32 +0100

    Fixes issue 841.

--- napi.h
+++ napi.h
@@ -34,6 +34,11 @@ static_assert(sizeof(char16_t) == sizeof(wchar_t), "Size mismatch between char16
   #endif
 #endif
 
+// VS2013 does not support noexcept
+#if defined(_MSC_VER) && _MSC_VER <= 1800
+#define _NOEXCEPT
+#endif
+
 #ifdef _NOEXCEPT
   #define NAPI_NOEXCEPT _NOEXCEPT
 #else
@@ -1747,13 +1752,13 @@ namespace Napi {
     static napi_value InstanceSetterCallbackWrapper(napi_env env, napi_callback_info info);
 
     template <InstanceSetterCallback method>
-    static napi_value WrappedMethod(napi_env env, napi_callback_info info) noexcept;
+    static napi_value WrappedMethod(napi_env env, napi_callback_info info) NAPI_NOEXCEPT;
 
     template <InstanceSetterCallback setter> struct SetterTag {};
 
     template <InstanceSetterCallback setter>
-    static napi_callback WrapSetter(SetterTag<setter>) noexcept { return &This::WrappedMethod<setter>; }
-    static napi_callback WrapSetter(SetterTag<nullptr>) noexcept { return nullptr; }
+    static napi_callback WrapSetter(SetterTag<setter>) NAPI_NOEXCEPT { return &This::WrappedMethod<setter>; }
+    static napi_callback WrapSetter(SetterTag<nullptr>) NAPI_NOEXCEPT { return nullptr; }
   };
 
   /// Base class to be extended by C++ classes exposed to JavaScript; each C++ class instance gets
@@ -1886,13 +1891,13 @@ namespace Napi {
                                  StaticSetterCallback> StaticAccessorCallbackData;
 
     template <StaticSetterCallback method>
-    static napi_value WrappedMethod(napi_env env, napi_callback_info info) noexcept;
+    static napi_value WrappedMethod(napi_env env, napi_callback_info info) NAPI_NOEXCEPT;
 
     template <StaticSetterCallback setter> struct StaticSetterTag {};
 
     template <StaticSetterCallback setter>
-    static napi_callback WrapStaticSetter(StaticSetterTag<setter>) noexcept { return &This::WrappedMethod<setter>; }
-    static napi_callback WrapStaticSetter(StaticSetterTag<nullptr>) noexcept { return nullptr; }
+    static napi_callback WrapStaticSetter(StaticSetterTag<setter>) NAPI_NOEXCEPT { return &This::WrappedMethod<setter>; }
+    static napi_callback WrapStaticSetter(StaticSetterTag<nullptr>) NAPI_NOEXCEPT { return nullptr; }
 
     bool _construction_failed = true;
   };
