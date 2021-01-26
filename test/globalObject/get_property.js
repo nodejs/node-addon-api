fe108015eb40db899b582fe82c35cd47ad5f9732
JckXia <ys3xia@uwaterloo.ca>
2021-01-25 21:03:52 -0500

    Add tests to check for error messages

--- test/globalObject/get_property.js
+++ test/globalObject/get_property.js
@@ -8,42 +8,53 @@ test(require(`../build/${buildType}/binding_noexcept.node`));
 
 
 function test(binding) {
-    const KEY_TYPE = {
-        NAPI:  'KEY_AS_NAPI_VALUE',
+  const KEY_TYPE = {
         C_STR: 'KEY_AS_C_STRING',
         CPP_STR: 'KEY_AS_CPP_STRING',
+        NAPI:  'KEY_AS_NAPI_VALUES',
         INT_32: 'KEY_AS_INT_32_NUM'
     };
-    binding.globalObject.createMockTestObject();
-    function assertGlobalObjectPropertyIs(key, attribute, keyType){
-        let napiObjectAttr;
-        switch(keyType)
-        {
-            case KEY_TYPE.NAPI:
-            napiObjectAttr = binding.globalObject.getPropertyWithNapiValue(key);
-            assert.deepStrictEqual(attribute, napiObjectAttr);
-            break;
 
-            case KEY_TYPE.C_STR:
-            napiObjectAttr = binding.globalObject.getPropertyWithCString(key);
-            assert.deepStrictEqual(attribute, napiObjectAttr);
-            break;
+  binding.globalObject.createMockTestObject();
+  function assertGlobalObjectPropertyIs(key, attribute, keyType) {
+    let napiObjectAttr;
+    switch(keyType)
+      {
+        case KEY_TYPE.NAPI:
+          napiObjectAttr = binding.globalObject.getPropertyWithNapiValue(key);
+          assert.deepStrictEqual(attribute, napiObjectAttr);
+          break;
 
-            case KEY_TYPE.CPP_STR:
-            napiObjectAttr = binding.globalObject.getPropertyWithCppString(key);
-            assert.deepStrictEqual(attribute, napiObjectAttr);
-            break;
+        case KEY_TYPE.C_STR:
+          napiObjectAttr = binding.globalObject.getPropertyWithCString(key);
+          assert.deepStrictEqual(attribute, napiObjectAttr);
+          break;
 
-            case KEY_TYPE.INT_32:
-            napiObjectAttr = binding.globalObject.getPropertyWithInt32(key);
-            assert.deepStrictEqual(attribute, napiObjectAttr);
-            break;
-        }
-    }
+        case KEY_TYPE.CPP_STR:
+          napiObjectAttr = binding.globalObject.getPropertyWithCppString(key);
+          assert.deepStrictEqual(attribute, napiObjectAttr);
+          break;
+
+        case KEY_TYPE.INT_32:
+          napiObjectAttr = binding.globalObject.getPropertyWithInt32(key);
+          assert.deepStrictEqual(attribute, napiObjectAttr);
+          break;
+      }
+  }
  
-    assertGlobalObjectPropertyIs('2',global['2'], KEY_TYPE.NAPI);
-    assertGlobalObjectPropertyIs('c_str_key',global['c_str_key'],KEY_TYPE.C_STR);
-    assertGlobalObjectPropertyIs('cpp_string_key',global['cpp_string_key'],KEY_TYPE.CPP_STR);
-    assertGlobalObjectPropertyIs('circular',global['circular'],KEY_TYPE.CPP_STR);   
-    assertGlobalObjectPropertyIs(15, global['15'], KEY_TYPE.INT_32); 
+  function assertErrMessageIsThrown(propertyFetchFunction, errMsg) {
+    assert.throws(() => {
+      propertyFetchFunction(undefined);    
+    }, errMsg);
+  }
+
+  assertGlobalObjectPropertyIs('2',global['2'], KEY_TYPE.NAPI);
+  assertGlobalObjectPropertyIs('c_str_key',global['c_str_key'],KEY_TYPE.C_STR);
+  assertGlobalObjectPropertyIs('cpp_string_key',global['cpp_string_key'],KEY_TYPE.CPP_STR);
+  assertGlobalObjectPropertyIs('circular',global['circular'],KEY_TYPE.CPP_STR);   
+  assertGlobalObjectPropertyIs(15, global['15'], KEY_TYPE.INT_32); 
+
+  assertErrMessageIsThrown(binding.globalObject.getPropertyWithCString, 'Error: A string was expected');
+  assertErrMessageIsThrown(binding.globalObject.getPropertyWithCppString, 'Error: A string was expected');
+  assertErrMessageIsThrown(binding.globalObject.getPropertyWithInt32, 'Error: A number was expected')
 }
