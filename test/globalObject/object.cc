dc1eb48314f3e29dde0731de74963e54805e64b0
JckXia <ys3xia@uwaterloo.ca>
2021-01-27 21:10:55 -0500

    Still running into the same issue

--- test/globalObject/object.cc
+++ test/globalObject/object.cc
@@ -45,6 +45,7 @@ Object InitGlobalObject(Env env) {
       Function::New(env, HasPropertyWithInt32AsKey);
   exports["hasPropertyWithCppStyleString"] =
       Function::New(env, HasPropertyWithCppStyleStringAsKey);
-  exports["hasPropertyWithNapiValueAsKey"] = Function::New(env, HasPropertyWithNapiValueAsKey);
+  exports["hasPropertyWithNapiValueAsKey"] =
+      Function::New(env, HasPropertyWithNapiValueAsKey);
   return exports;
 }
