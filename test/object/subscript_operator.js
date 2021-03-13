8e2ee12daedff316f3c0ff1316bae820b7c375cb
JoseExposito <jose.exposito89@gmail.com>
2021-03-13 14:23:34 +0100

    Test that Object::operator[] overrides object keys by index

--- test/object/subscript_operator.js
+++ test/object/subscript_operator.js
@@ -15,4 +15,5 @@ function test(binding) {
   testProperty({}, 'key', 'value', binding.object.subscriptGetWithCStyleString, binding.object.subscriptSetWithCStyleString);
   testProperty({ key: 'override me' }, 'key', 'value', binding.object.subscriptGetWithCppStyleString, binding.object.subscriptSetWithCppStyleString);
   testProperty({}, 0, 'value', binding.object.subscriptGetAtIndex, binding.object.subscriptSetAtIndex);
+  testProperty({ key: 'override me' }, 0, 'value', binding.object.subscriptGetAtIndex, binding.object.subscriptSetAtIndex);
 }
