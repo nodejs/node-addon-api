599146e6afe15173714933cafd9d432f4e2163b9
Doni Rubiagatra <doni@zero-one-group.com>
2021-09-11 08:02:22 +0700

    lint: add eslint based on config-semistandard

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  }
+}
