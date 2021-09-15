071752ceb92f621f5e966a62e10b8af6fb6cac64
Doni Rubiagatra <doni@zero-one-group.com>
2021-09-15 09:24:06 +0700

    test

--- test/addon.js
+++ test/addon.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 module.exports = require('./common').runTest(test);
 
-function test(binding) {
+function test (binding) {
   assert.strictEqual(binding.addon.increment(), 43);
   assert.strictEqual(binding.addon.increment(), 44);
   assert.strictEqual(binding.addon.subObject.decrement(), 43);
