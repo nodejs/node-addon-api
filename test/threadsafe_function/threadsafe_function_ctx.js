359ad0b2a38dee95410e71f04f03a379676bcced
JckXia <ys3xia@uwaterloo.ca>
2022-12-15 20:07:34 -0500

    test: Add coverage for ThreadSafeFunction new overloads

--- test/threadsafe_function/threadsafe_function_ctx.js
+++ test/threadsafe_function/threadsafe_function_ctx.js
@@ -9,4 +9,6 @@ async function test (binding) {
   const tsfn = new binding.threadsafe_function_ctx.TSFNWrap(ctx);
   assert(tsfn.getContext() === ctx);
   await tsfn.release();
+  binding.threadsafe_function_ctx.AssertFnReturnCorrectCxt();
+  // console.log(binding.threadsafe_function_ctx);
 }
