5d39974057cdf4eec456219dd51497e8faf95897
JckXia <ys3xia@uwaterloo.ca>
2022-12-23 16:24:03 -0500

    test: Add unit test covg for callbackscopes

--- test/callbackscope.cc
+++ test/callbackscope.cc
@@ -3,6 +3,7 @@
 using namespace Napi;
 
 #if (NAPI_VERSION > 2)
+
 namespace {
 
 static void RunInCallbackScope(const CallbackInfo& info) {
@@ -12,11 +13,25 @@ static void RunInCallbackScope(const CallbackInfo& info) {
   callback.Call({});
 }
 
-}  // end anonymous namespace
+static void RunInCallbackScopeFromExisting(const CallbackInfo& info) {
+  Function callback = info[0].As<Function>();
+  Env env = info.Env();
+
+  AsyncContext ctx(env, "existing_callback_scope_test");
+  napi_callback_scope scope;
+  napi_open_callback_scope(env, Object::New(env), ctx, &scope);
+
+  CallbackScope existingScope(env, scope);
+  callback.Call({});
+}
+
+}  // namespace
 
 Object InitCallbackScope(Env env) {
   Object exports = Object::New(env);
   exports["runInCallbackScope"] = Function::New(env, RunInCallbackScope);
+  exports["runInPreExistingCbScope"] =
+      Function::New(env, RunInCallbackScopeFromExisting);
   return exports;
 }
 #endif
