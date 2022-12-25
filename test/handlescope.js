35d9d669b37c04130df6d3d9027ad9bad0a60095
JckXia <ys3xia@uwaterloo.ca>
2022-12-25 17:44:36 -0500

    test: add test covg for handle and escapehandle scopes
    PR-URL: https://github.com/nodejs/node-addon-api/pull/1263
    Reviewed-By: Michael Dawson <midawson@redhat.com

--- test/handlescope.js
+++ test/handlescope.js
@@ -6,7 +6,9 @@ module.exports = require('./common').runTest(test);
 
 function test (binding) {
   assert.strictEqual(binding.handlescope.createScope(), 'scope');
+  assert.strictEqual(binding.handlescope.createScopeFromExisting(), 'existing_scope');
   assert.strictEqual(binding.handlescope.escapeFromScope(), 'inner-scope');
+  assert.strictEqual(binding.handlescope.escapeFromExistingScope(), 'inner-existing-scope');
   assert.strictEqual(binding.handlescope.stressEscapeFromScope(), 'inner-scope999999');
   assert.throws(() => binding.handlescope.doubleEscapeFromScope(),
     Error,
