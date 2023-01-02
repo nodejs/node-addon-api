74febbaf1c9218f99ab007eb6cf3db54f88a3294
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2023-01-02 17:20:28 +0100

    Fix test for Node 14, 16

--- test/object/object_type_tag.js
+++ test/object/object_type_tag.js
@@ -1,3 +1,4 @@
+// @ts-check
 'use strict';
 
 const assert = require('assert');
@@ -8,8 +9,35 @@ module.exports = require('../common').runTest(test);
 function test ({ object_type_tag }) {
   const obj1 = object_type_tag.typedTaggedInstance(0);
   const obj2 = object_type_tag.typedTaggedInstance(1);
+
+  // Verify that type tags are correctly accepted.
+  assert.strictEqual(object_type_tag.checkTypeTag(0, obj1), true);
+  assert.strictEqual(object_type_tag.checkTypeTag(1, obj2), true);
+
+  // Verify that wrongly tagged objects are rejected.
+  assert.strictEqual(object_type_tag.checkTypeTag(0, obj2), false);
+  assert.strictEqual(object_type_tag.checkTypeTag(1, obj1), false);
+
+  // Verify that untagged objects are rejected.
+  assert.strictEqual(object_type_tag.checkTypeTag(0, {}), false);
+  assert.strictEqual(object_type_tag.checkTypeTag(1, {}), false);
+
+  // Node v14 and v16 have an issue checking type tags if the `upper` in
+  // `napi_type_tag` is 0, so these tests can only be performed on Node version
+  // >=18. See:
+  // - https://github.com/nodejs/node/issues/43786
+  // - https://github.com/nodejs/node/pull/43788
+  const nodeVersion = parseInt(process.versions.node.split('.')[0]);
+  if (nodeVersion < 18) {
+    return;
+  }
+
   const obj3 = object_type_tag.typedTaggedInstance(2);
   const obj4 = object_type_tag.typedTaggedInstance(3);
+
+  // Verify that untagged objects are rejected.
+  assert.strictEqual(object_type_tag.checkTypeTag(0, {}), false);
+  assert.strictEqual(object_type_tag.checkTypeTag(1, {}), false);
 
   // Verify that type tags are correctly accepted.
   assert.strictEqual(object_type_tag.checkTypeTag(0, obj1), true);
@@ -25,8 +53,4 @@ function test ({ object_type_tag }) {
   assert.strictEqual(object_type_tag.checkTypeTag(2, obj4), false);
   assert.strictEqual(object_type_tag.checkTypeTag(3, obj3), false);
   assert.strictEqual(object_type_tag.checkTypeTag(4, obj3), false);
-
-  // Verify that untagged objects are rejected.
-  assert.strictEqual(object_type_tag.checkTypeTag(0, {}), false);
-  assert.strictEqual(object_type_tag.checkTypeTag(1, {}), false);
 }
